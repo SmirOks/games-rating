e48a24877bbd9d0d0cabaf83c779c082d0d34206
SmirOks <149386824+SmirOks@users.noreply.github.com>
2024-04-27 21:43:03 +0300

    test2

---
+++ app.js
@@ -0,0 +1,30 @@
+// ...импорты и настройки
+const http = require("http");
+const path = require("path");
+const {
+  mainRouteController,
+  defaultRouteController,
+  gameRouteController,
+  voteRouteController,
+} = require("./controllers");
+
+const PORT = 3005;
+const server = http.createServer((req, res) => {
+  const url = req.url;
+  switch (url) {
+    case "/":
+      mainRouteController(res, "/index.html", ".html");
+      break;
+    // ...другие маршруты
+    case "/game":
+      gameRouteController(res);
+      break;
+    case "/vote":
+      voteRouteController(req, res);
+      break;
+    default:
+      defaultRouteController(res, url);
+  }
+});
+
+server.listen(PORT);
---
+++ controllers/index.js
@@ -0,0 +1,10 @@
+const defaultRouteController = require("../controllers/default.js");
+const gameRouteController = require("../controllers/game.js");
+const mainRouteController = require("../controllers/main.js");
+const voteRouteController = require("../controllers/vote.js");
+module.exports = {
+  defaultRouteController,
+  gameRouteController,
+  mainRouteController,
+  voteRouteController,
+};
